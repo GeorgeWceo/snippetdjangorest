2a1152a03c698eeb1c6b85d1c3c9e48e058946eb
Kenneth Kinyanjui <kenmbugua@gmail.com>
2015-12-02 22:29:34 +0300

    adding missing arguments

--- snippets/views.py
+++ snippets/views.py
@@ -11,7 +11,7 @@ class JSONResponse(HttpResponse):
     An HttpResponse that renders its content into JSON
     """
 
-    def __init__(self):
+    def __init__(self, data, **kwargs):
         content = JSONRenderer().render(data)
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
