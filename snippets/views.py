b99469c417ce645f90fce8fd9407719e90b37c36
Kenneth Kinyanjui <kenmbugua@gmail.com>
2015-12-07 22:38:43 +0300

    added Mixins for the SnippetList CBV

--- snippets/views.py
+++ snippets/views.py
@@ -1,33 +1,31 @@
 from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import mixins
+from rest_framework import generics
 from .models import Snippet
 from .serializers import SnippetSerializer
 
 
-class SnippetList(APIView):
+class SnippetList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
     """
     List all snippets , or create a new snippet
     """
-    def get(self, request, format=None):
+
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
         """
         handles GET requests
         """
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request, format=None):
+    def post(self, request, *args, **kwargs):
         """
         handles POST requests
         """
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+        return self.create(request, *args, **kwargs)
 
 
 class SnippetDetail(APIView):
@@ -37,6 +35,7 @@ class SnippetDetail(APIView):
     :param pk:
     :return:
     """
+
     def get_object(self, pk):
 
         try:
@@ -44,12 +43,12 @@ class SnippetDetail(APIView):
         except Snippet.DoesNotExist:
             raise Http404
 
-    def get(self,request,pk, format=None):
+    def get(self, request, pk, format=None):
         snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    def put(self, request , pk, format=None):
+    def put(self, request, pk, format=None):
         snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
@@ -61,5 +60,3 @@ class SnippetDetail(APIView):
         snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
